aafc63f40427fd4ba436e74520fe7cfe0474f4d3
Crowdbotics <team@crowdbotics.com>
2021-08-05 17:07:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "steep_sky_29521Identifier",
+  url: "https://steep_sky_29521Identifier.botics.co",
+  api: "https://steep_sky_29521Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
